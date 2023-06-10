99decc042ee25cb79629d69c45d3efc93c6e5680
Amit <67889816+amit8171-pro@users.noreply.github.com>
2023-06-10 17:51:14 +0545

    searchfiled changed

--- src/pages/searchresult/Searchresult.jsx
+++ src/pages/searchresult/Searchresult.jsx
@@ -3,7 +3,7 @@ import { useParams } from 'react-router-dom';
 import InfiniteScroll from 'react-infinite-scroll-component';
 import './style.scss';
 import { fetchDataFromApi } from '../../utils/api';
-import ContentWrapper from '../../components/contentWrapper/ContentWrapper';
+import ContentWrapper from '../../components/contentwrapper/ContentWrapper';
 import Spinner from '../../components/spinner/Spinner';
 import MovieCard from '../../components/movieCard/MovieCard';
 
